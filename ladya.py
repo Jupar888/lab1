b85218ba4731b91e43e39f2d789258342d9adb90
Jupar888 <j.jumagaziev@yandex.kz>
2022-11-19 22:16:07 +0500

    all tasks

---
+++ ladya.py
@@ -0,0 +1,7 @@
+x,y = int(input()),int(input())
+x2,y2 = int(input()),int(input())
+
+if x == x2 or y == y2:
+    print('YES')
+else:
+    print('NO')
--- maximumofnums.py
+++ maximumofnums.py
@@ -2,7 +2,7 @@ d = int(input())
 e = int(input())
 f = int(input())
 
-if d >e and d > f:
+if d > e and d > f:
     print(d)
 elif e > d and e > f:
     print(e)
